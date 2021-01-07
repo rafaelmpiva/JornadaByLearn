90764f1408699254dcaa2cd456a3f4fdd24f979f
rafaelmpiva <75317155+rafaelmpiva@users.noreply.github.com>
2021-01-07 20:05:05 -0300

    Add files via upload

---
+++ def atividade(horario,clima,temperatura).py
@@ -0,0 +1,11 @@
+def atividade(horario,clima,temperatura):
+
+    if horario == 'manha' or horario == 'tarde':
+        if clima == 'ensolarado' and temperatura = 'quente':
+            print("Uma piscina cairia bem")
+
+    else:
+        print("Melhor estudar python de qualquer maneira")
+
+
+atividade('manha','chuvoso','frio')
